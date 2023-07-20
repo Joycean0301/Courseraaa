ec60fcab12af638b87d1af95f34309baa15de810
Trung Kien <103662477+Joycean0301@users.noreply.github.com>
2023-07-20 10:09:26 +0700

    Update background.js

--- background.js
+++ background.js
@@ -5,5 +5,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, { file: "feature_fill.js" });
   });
-  
-  
